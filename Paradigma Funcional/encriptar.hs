9d64a010bc7aa04e8ac7afee8840f37fdb326d81
fash2110 <Fash2110@gmail.com>
2025-05-14 13:08:02 -0600

    Encripción

---
+++ Paradigma Funcional/encriptar.hs
@@ -0,0 +1,29 @@
+import Data.Hashable
+
+encriptar :: String -> String
+--retorna el string hasheado
+encriptar msj = show (hash msj) 
+
+separar :: String -> (String, String)
+--Separa el string cuando encuentra un ;
+separar xs = let (a, b) = break (== ';') xs
+                in (a, drop 1 b)
+
+esValido :: String -> String -> FilePath -> IO Bool
+-- Recibe un usuario y contraseña y verifica si son correctos -> True: Acceso habilitado
+--                                                            -> False: Acceso inhabilitado
+esValido "" _ _ = return False --Usuario vacio
+esValido _ "" _ = return False --Contrasenna vacia
+esValido _ _ "" = return False -- Archivo vacio
+esValido usuario contra archivo = do
+    cuerpo <- readFile archivo
+    let lineas = lines cuerpo
+        lista = map separar lineas
+        uHash = encriptar usuario
+        cHash = encriptar contra
+    return $ any (\(u, c) -> u == uHash && c == cHash) lista
+
+--Ejemplos de como usar las funciones                           
+--  print (encriptar "Hola")
+--  valido <- esValido "Hola" "Hola" "usuarios.txt"
+--  print valido

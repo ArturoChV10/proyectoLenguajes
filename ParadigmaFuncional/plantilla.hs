3c5c30d152184e98fd7c43eab1ff1f80310d20c6
ArturoChV10 <chavarriavillareviajorgearturo@gmail.com>
2025-05-15 01:43:11 -0600

    Se completó el guardado de archivos de servicios, falta adaptar el nombre para que el archivo creado corresponda al nombre de cada usuario y hacer que la encriptación funcione enn relación a esto

--- ParadigmaFuncional/plantilla.hs
+++ ParadigmaFuncional/plantilla.hs
@@ -1,4 +1,6 @@
 import System.IO
+import Data.List (isInfixOf)
+import System.Directory (removeFile, renameFile)
 
 main :: IO ()
 main = mainLoop
@@ -13,7 +15,7 @@ mainLoop = do
     putStr "Seleccione una opción: "
     hFlush stdout
     opcion <- getLine
-    
+
     -- Opcion para crear un nuevo usuario
     case opcion of
         "1" -> do
@@ -21,23 +23,60 @@ mainLoop = do
             username <- getLine
             putStr "Contraseña: "
             password <- getLine
+            archivo <- openFile "usuarios.txt" AppendMode
+            crearArchivo username password archivo
+            hClose archivo
             mainLoop
-    
+
     -- Opcion para ingresar a cuenta de usuario
         "2" -> do
-            putStr "Nombre de usuario existente: "
+            putStrLn "Nombre de usuario existente: "
             username <- getLine
-            putStr "Contraseña del usuario: "
+            putStrLn "Contraseña del usuario: "
             password <- getLine
             loopGestion
-            
+
         "3" -> do
             putStrLn "Saliendo del programa..."
-            
+
         _ -> do
             putStrLn "Opción no válida, intente nuevamente"
             mainLoop
-            
+
+crearArchivo :: String -> String -> Handle -> IO ()
+crearArchivo name password archivo = do
+  hPutStrLn archivo (name ++ ";" ++ password)
+
+consultarServicio :: String -> Handle -> IO ()
+consultarServicio name archivo = do
+    completo <- hIsEOF archivo
+    if completo
+        then do
+            putStrLn "No se encontró el servicio"
+            return ()
+        else do
+            linea <- hGetLine archivo
+            if name `isInfixOf` linea -- name está en linea
+                then do
+                    putStrLn linea
+                    return ()
+                else consultarServicio name archivo
+
+eliminarServicio :: String -> Handle -> Handle -> IO ()
+eliminarServicio name oldFile newFile = do
+  completo <- hIsEOF oldFile
+  if completo
+    then return ()
+    else do
+        linea <- hGetLine oldFile
+        let coincide = name `isInfixOf` linea
+        if coincide
+            then eliminarServicio name oldFile newFile
+            else do
+                hPutStrLn newFile linea
+                eliminarServicio name oldFile newFile
+
+
 loopGestion :: IO ()
 loopGestion = do
     putStrLn "\n=== Gestión de Contraseñas ==="
@@ -50,39 +89,73 @@ loopGestion = do
     putStr "Seleccione una opción: "
     hFlush stdout
     opcion <- getLine
-    
+
     -- Opcion para agregar un nuevo usuario
     case opcion of
         "1" -> do
-            putStr "Nombre del nuevo servicio: "
+            putStrLn "Nombre del nuevo servicio: "
             service <- getLine
-            putStr "Contraseña: "
+            putStrLn "Contraseña: "
             password <- getLine
+            archivo <- openFile "servicios.txt" AppendMode
+            crearArchivo service password archivo
+            hClose archivo
             loopGestion
-    
+
     -- Opcion para consultar cuenta de usuario
         "2" -> do
             putStr "Nombre del servicio a consultar: "
             nombre <- getLine
+            archivo <- openFile "servicios.txt" AppendMode
+            consultarServicio nombre archivo
+            hClose archivo
             loopGestion
-            
+
     -- Opción para modificar los datos de un servicio
         "3" -> do
-            putStr "Nombre del servicio a modificar: "
+            putStrLn "Nombre del servicio a modificar: "
             service <- getLine
-            putStr "Nueva contraseña: "
+            putStrLn "Nueva contraseña: "
             password <- getLine
+
+            -- Eliminar el servicio antiguo
+            archivoViejo <- openFile "servicios.txt" ReadMode
+            archivoNuevo <- openFile "servicios.tmp" WriteMode
+            eliminarServicio service archivoViejo archivoNuevo
+            hClose archivoViejo
+            hClose archivoNuevo
+            
+            removeFile "servicios.txt"
+            renameFile "servicios.tmp" "servicios.txt"
+
+            -- Agregar el archivo nuevo
+            archivo <- openFile "servicios.txt" AppendMode
+            crearArchivo service password archivo
+            hClose archivo
+
             loopGestion
 
     -- Opción para eliminar los datos de un servicio
         "4" -> do
-            putStr "Nombre del servicio a eliminar: "
+            putStrLn "Nombre del servicio a eliminar: "
             service <- getLine
-            loopGestion
             
+            archivoViejo <- openFile "servicios.txt" ReadMode
+            archivoNuevo <- openFile "servicios.tmp" WriteMode
+            eliminarServicio service archivoViejo archivoNuevo
+            hClose archivoViejo
+            hClose archivoNuevo
+            
+            removeFile "servicios.txt"
+            renameFile "servicios.tmp" "servicios.txt"
+
+            loopGestion
+
+    -- Opción para regresar al menú principal
         "5" -> do
             mainLoop
-            
+
         _ -> do
             putStrLn "Opción no válida, intente nuevamente"
+            loopGestion
 
